80771b6c3f676a5790f0e59590855b2961374263
sabbirshawon <sabbirs93@gmail.com>
2020-10-05 12:05:55 +0600

    email+usaZipCode+PhoneValidation methods added

---
+++ script.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+			
+
+    $.validator.addMethod("emailValid", function(value, element)  {
+
+        if (/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(value)) {
+            return true;
+        } else {
+            return false;
+        }
+    }, "Email address is not valid");
+
+
+
+    $.validator.addMethod("zipcode", function(value, element) {
+        return this.optional(element) || /^\d{5}(?:-\d{4})?$/.test(value);
+      }, "Please enter a valid USA postcode");
+
+
+    $.validator.addMethod("phoneValid", function(value, element) {
+    return this.optional(element) || /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im.test(value);
+    });
+      
+
+});
